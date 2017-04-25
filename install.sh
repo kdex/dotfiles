fd058c2f0a2966b1c1d5ca8fc866c0331199d8b6
kdex <kdex@kdex.de>
2017-04-25 06:37:25 +0200

    Small edits, uploader

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 REPOSITORY=$(dirname "$(readlink -f "$0")")
 DIRECTORY_GIT_TEMPLATE=".git-template"
+cd "$REPOSITORY"
 # Setup git
 git config --global user.name "kdex"
 git config --global user.email "kdex@kdex.de"
@@ -11,7 +12,7 @@ cp -aT $DIRECTORY_GIT_TEMPLATE ~/$DIRECTORY_GIT_TEMPLATE
 # Setup keyboard
 cp .Xmodmap .xinitrc ~/
 # Setup shell
-cp .bashrc .bash_profile .inputrc ~/
+cp .bashrc .bash_profile .inputrc .zshrc .zsh_profile ~/
 # Setup nano
 git clone https://github.com/nanorc/nanorc.git /tmp/nanorc
 cd /tmp/nanorc && make install && cd "$REPOSITORY"
@@ -19,3 +20,7 @@ cp .nanorc ~/
 rm -rf /tmp/nanorc
 # Setup KDE
 cp -aT .config ~/
+# Copy scripts
+for binary in .bin/*; do
+	cp "$binary" ~/.local/bin
+done
