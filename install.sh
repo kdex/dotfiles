0a4d232943a13114e9ed33f73de307fe9180c93a
kdex <kdex@kdex.de>
2018-01-25 16:21:27 +0100

    Added .npmrc, .rtorrentrc

--- install.sh
+++ install.sh
@@ -2,23 +2,25 @@
 REPOSITORY=$(dirname "$(readlink -f "$0")")
 DIRECTORY_GIT_TEMPLATE=".git-template"
 cd "$REPOSITORY"
-# Setup git
+# Set up git
 git config --global user.name "kdex"
 git config --global user.email "kdex@kdex.de"
 git config --global core.editor "nano"
 git config --global push.default "simple"
 git config --global commit.gpgsign true
-git config --global init.templatedir ~/$DIRECTORY_GIT_TEMPLATE
-cp -aT $DIRECTORY_GIT_TEMPLATE ~/$DIRECTORY_GIT_TEMPLATE
-# Setup keyboard
+git config --global init.templatedir "~/$DIRECTORY_GIT_TEMPLATE"
+cp -aT "$DIRECTORY_GIT_TEMPLATE" "~/$DIRECTORY_GIT_TEMPLATE"
+# Set up keyboard
 cp .Xmodmap .xinitrc ~/
-# Setup shell
+# Set up shell
 cp .bashrc .bash_profile .inputrc .zshrc .zsh_profile ~/
-# Setup nano
+# Set up nano
 git clone https://github.com/nanorc/nanorc.git /tmp/nanorc
 cd /tmp/nanorc && make install && cd "$REPOSITORY"
 cp .nanorc ~/
 rm -rf /tmp/nanorc
-# Setup KDE
+# Set up .npmrc
+echo "sign-git-tag=true" >> ~/.npmrc
+# Set up KDE
 cp -aT .config/. ~/.config
 cp -aT .local ~/.local
